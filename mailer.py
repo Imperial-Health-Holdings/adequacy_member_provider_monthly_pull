3630277974619378c8e60774327de970373070b6
Henry Cheng <Henry.Cheng@imperialhealthholdings.com>
2021-12-01 11:04:43 -0800

    add function: get_recipient_list

--- mailer.py
+++ mailer.py
@@ -1,17 +1,30 @@
 import win32com.client as win32
 
+def get_recipient_list(recipient:str) -> str:
+    '''
+    This function takes repicient email list from config_mailer file
+    and process it into required format.
+
+    Input from config_mailer file
+        abc@imperialhealthholdings.com
+        xyz@imperialhealthholdings.com
+    Output from function
+        abc@imperialhealthholdings.com; xyz@imperialhealthholdings.com
+    '''
+    recipient_list = [i.strip() for i in recipient.splitlines()]
+    recipient_list = filter(None, item) # incase there's empty line, filter them out
+    recipient_list = '; '.join(item)
+
+    return recipient_list
+
 def send_report_notification(recipient:str, subject:str, text:str) -> None:
     '''
-    This function sends email using local Outlook account.
-    
-    For multiple recipients, each address is separated by "; ".
-    For example, 
-        Joe.smith@imperialhealthholdings.com; James.johnson@imperialhealthholdings.com
+    This function sends email using local Outlook account
     '''
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
 
-    mail.To = recipient
+    mail.To = get_recipient_list(recipient)
     mail.Subject = subject
     mail.Body = text
     #mail.HTMLBody = '<h2>HTML Message body</h2>'
--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import configparser
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('config_mailer.ini')
 
-text = config['MAILER']['text']
+text = config['RECIPIENT']['recipient']
+
+# Need a function to read recipient list and put into string seperated by "; "
+item = [i.strip() for i in text.splitlines()]
+item = filter(None, item)
+item2 = '; '.join(item)
 
 print(text)
+
+print(item)
+
+print(item2)
