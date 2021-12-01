29af0f52a4b7bc6581de5c147f99de4162923a26
Henry Cheng <Henry.Cheng@imperialhealthholdings.com>
2021-12-01 08:35:16 -0800

    add pause

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 @echo off
 "C:\Users\henry.cheng\Desktop\WIP\Project\Analytics\adequacy_member_provider_monthly_pull\env\Scripts\python.exe" "C:\Users\henry.cheng\Desktop\WIP\Project\Analytics\adequacy_member_provider_monthly_pull\program\main.py"
+pause
