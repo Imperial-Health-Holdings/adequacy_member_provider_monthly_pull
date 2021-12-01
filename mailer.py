ed8a3e4ce4b28fc544eb4400a8f6eed21550fdaf
Henry Cheng <Henry.Cheng@imperialhealthholdings.com>
2021-12-01 10:12:48 -0800

    wrap into function, add documentation to the function

--- mailer.py
+++ mailer.py
@@ -1,14 +1,28 @@
 import win32com.client as win32
 
-outlook = win32.Dispatch('outlook.application')
-mail = outlook.CreateItem(0)
-mail.To = 'To address'
-mail.Subject = 'Message subject'
-mail.Body = 'Message body'
-mail.HTMLBody = '<h2>HTML Message body</h2>' #this field is optional
+def send_report_notification(send_to, subject, body):
+    '''
+    This function sends email using local Outlook account.
+    
+    For multiple recipients, each address is separated by "; ".
+    For example, 
+        Joe.smith@imperialhealthholdings.com; James.johnson@imperialhealthholdings.com
+    '''
 
-# To attach a file to the email (optional):
-attachment  = "Path to the attachment"
-mail.Attachments.Add(attachment)
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
 
-mail.Send()
+    mail.To = 'henry.cheng@imperialhealthholdings.com'
+    mail.Subject = 'Meail Tester from Python'
+    mail.Body = 'Testing message'
+    #mail.HTMLBody = '<h2>HTML Message body</h2>'
+
+
+    print('Process complete.')
+
+    # To attach a file to the email (optional):
+    if False:
+        attachment  = "Path to the attachment"
+        mail.Attachments.Add(attachment)
+
+    mail.Send()
