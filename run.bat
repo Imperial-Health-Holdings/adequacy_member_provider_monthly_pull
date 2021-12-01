5361e3e7537e13c0302d8cc966a0d53e12bd207d
Henry Cheng <Henry.Cheng@imperialhealthholdings.com>
2021-12-01 08:24:39 -0800

    initial commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Users\henry.cheng\Desktop\WIP\Project\Analytics\adequacy_member_provider_monthly_pull\env\Scripts\python.exe" "C:\Users\henry.cheng\Desktop\WIP\Project\Analytics\adequacy_member_provider_monthly_pull\program\main.py"
